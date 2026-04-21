87d59b0d1ef99f82a65b37f61171b7ef072ae6b5
Kinaetron <darren.daley@hotmail.co.uk>
2026-04-21 19:03:38 +0100

    first pass at setting up a window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,47 @@
-#include <stdio.h>
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main(void) 
+static SDL_Window* window = NULL;
+static SDL_Renderer* renderer = NULL;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
-    printf("Start of my chip8 project");
+	SDL_SetAppMetadata("Chip 8 Emulator", "1.0", "com.chip8-emulator");
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	if (!SDL_CreateWindowAndRenderer("Chip 8 Emulator", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) 
+	{
+		SDL_Log("Couldn't create window/emulator: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	return SDL_APP_CONTINUE;
 }
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+	if (event->type == SDL_EVENT_QUIT) {
+		return SDL_APP_SUCCESS;
+	}
+
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+	SDL_SetRenderDrawColorFloat(renderer, 0.392f, 0.584f, 0.929f, SDL_ALPHA_OPAQUE_FLOAT);
+
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+
+	return SDL_APP_CONTINUE;
+}
+
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) { }
