b15f62413255c4aa6b7c43293fa08d0b968354ac
Kinaetron <darren.daley@hotmail.co.uk>
2026-04-21 20:50:50 +0100

    changed to SDL GPU
    I changed over from using SDL Renderer to using the SDL GPU backend

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
+#include <stdbool.h>
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_gpu.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_log.h>
 
 static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
+static SDL_GPUDevice* gpu_device = NULL;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
@@ -14,11 +20,42 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 		return SDL_APP_FAILURE;
 	}
 
-	if (!SDL_CreateWindowAndRenderer("Chip 8 Emulator", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) 
+
+	window = SDL_CreateWindow("Chip 8 Emulator", 640, 480, SDL_WINDOW_RESIZABLE);
+	if (window == NULL)
 	{
-		SDL_Log("Couldn't create window/emulator: %s", SDL_GetError());
+		SDL_Log("Couldn't create window: %s", SDL_GetError());
 		return SDL_APP_FAILURE;
 	}
+
+	SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+	SDL_ShowWindow(window);
+
+	gpu_device = SDL_CreateGPUDevice(
+		SDL_GPU_SHADERFORMAT_SPIRV | 
+		SDL_GPU_SHADERFORMAT_DXIL | 
+		SDL_GPU_SHADERFORMAT_MSL | 
+		SDL_GPU_SHADERFORMAT_METALLIB, 
+		true, 
+		NULL);
+
+	if (gpu_device == NULL) 
+	{
+		SDL_Log("Error: SDL_CreateGPUDevice: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	if (!SDL_ClaimWindowForGPUDevice(gpu_device, window))
+	{
+		SDL_Log("Error: SDL_CreateGPUDevice Window Claim: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	SDL_SetGPUSwapchainParameters(
+		gpu_device, 
+		window, 
+		SDL_GPU_SWAPCHAINCOMPOSITION_SDR, 
+		SDL_GPU_PRESENTMODE_VSYNC);
 
 	return SDL_APP_CONTINUE;
 }
@@ -34,11 +71,43 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
-	SDL_SetRenderDrawColorFloat(renderer, 0.392f, 0.584f, 0.929f, SDL_ALPHA_OPAQUE_FLOAT);
+	SDL_GPUCommandBuffer* cmdbuf = SDL_AcquireGPUCommandBuffer(gpu_device);
+	if (cmdbuf == NULL)
+	{
+		SDL_Log("AcquireGPUCommandBuffer failed: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
 
-	SDL_RenderClear(renderer);
+	SDL_GPUTexture* swapchainTexture;
+	if (!SDL_WaitAndAcquireGPUSwapchainTexture(
+		cmdbuf, 
+		window, 
+		&swapchainTexture, 
+		NULL, 
+		NULL)) 
+	{
+		SDL_Log("WaitAndAcquireGPUSwapchainTexture failed: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
 
-	SDL_RenderPresent(renderer);
+	if (swapchainTexture != NULL)
+	{
+		SDL_GPUColorTargetInfo colorTargetInfo = { 0 };
+		colorTargetInfo.texture = swapchainTexture;
+		colorTargetInfo.clear_color = (SDL_FColor){ 0.3f, 0.4f, 0.5f, 1.0f };
+		colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
+		colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
+
+		SDL_GPURenderPass* renderPass = SDL_BeginGPURenderPass(
+			cmdbuf, 
+			&colorTargetInfo, 
+			1, 
+			NULL);
+
+		SDL_EndGPURenderPass(renderPass);
+	}
+
+	SDL_SubmitGPUCommandBuffer(cmdbuf);
 
 	return SDL_APP_CONTINUE;
 }
