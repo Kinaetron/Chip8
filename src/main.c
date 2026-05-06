97cc1c304838b614f442db77033e66452960d3f6
Kinaetron <darren.daley@hotmail.co.uk>
2026-05-06 15:34:23 +0100

    loading up the font at initalization

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_log.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_events.h>
