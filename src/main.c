b97b0dda966a399ce0b8e3bd46d0d97f67d207c8
Kinaetron <darren.daley@hotmail.co.uk>
2026-05-19 15:27:19 +0100

    the frame rate is now limited to 60

--- src/main.c
+++ src/main.c
@@ -1,18 +1,22 @@
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_gpu.h>
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_timer.h>
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_events.h>
 
+#include <stdint.h>
 #include <malloc.h>
 
 #include "ui.h"
 #include "context.h"
 #include "renderer.h"
-#include <SDL3/SDL_gpu.h>
-#include <SDL3/SDL_video.h>
+
+#define MS_PER_FRAME (1000.0 / 60.0)
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
@@ -63,15 +67,30 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
+	uint64_t start_time = SDL_GetPerformanceCounter();
 	Context* context = (Context*)appstate;
 
 	chip8_cycle(context->state);
 
-	return Render(
+	SDL_AppResult render_result = Render(
 		context->gpu_device,
 		context->graphicsContext,
 		context->window,
 		context->state);
+
+	if (render_result == SDL_APP_FAILURE) {
+		return SDL_APP_FAILURE;
+	}
+
+	uint64_t end_time = SDL_GetPerformanceCounter();
+	double elapsed_ms = (double)(end_time - start_time) * 1000.0 / SDL_GetPerformanceFrequency();
+
+	double remaining_ms = MS_PER_FRAME - elapsed_ms;
+	if (remaining_ms > 0) {
+		SDL_Delay((uint32_t)remaining_ms);
+	}
+
+	return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) 
